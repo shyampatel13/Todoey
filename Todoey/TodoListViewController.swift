e3ab36a2273251a6b7422b47cb740762fcedb0b1
iOS Swift <iOSSwift@shyams-macbook-pro.local>
2018-01-04 12:50:37 -0500

    Add new item functionality complete.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -12,7 +12,7 @@ class TodoListViewController: UITableViewController {
 
     
     
-    let itemArray = ["Find Mike", "Buy Eggos", "Dig Holes"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Dig Holes"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,17 +39,43 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {          
         
-            if         tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-                tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        if         tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .none
 
-            } else {
-                tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-            }
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
         
-            tableView.deselectRow(at: indexPath, animated: true)
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
     
+    //MARK - Add new items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var newItem = UITextField()
+        
+        let alert = UIAlertController(title: "Add new Todoey item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add item", style: .default) { (action) in
+            //what will happen when the user clicks the add item button on the UIAlert.
+            
+            print("Success!")
+            self.itemArray.append(newItem.text!)
+            
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            newItem = alertTextField
+        }
+        
+        alert.addAction(action)
+
+        present(alert, animated: true, completion: nil)
+    }
     
     
     
